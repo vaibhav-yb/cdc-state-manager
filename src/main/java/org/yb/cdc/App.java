c5cad255692a1c5f446ec3889ee3bb86644fb0b0
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2022-09-17 05:02:10 +0000

    modifications

--- src/main/java/org/yb/cdc/App.java
+++ src/main/java/org/yb/cdc/App.java
@@ -6,8 +6,28 @@ package org.yb.cdc;
  */
 public class App 
 {
+    private final CmdLineOpts configuration;
+    private final Functions functions;
+    
+    public App(CmdLineOpts configuration) {
+        this.configuration = configuration;
+        this.functions = new Functions(configuration);
+    }
+
+    public void run() {
+        try {
+            functions.execute();
+        } catch (Exception e) {
+            System.out.println("Exception thrown while executing");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        CmdLineOpts configuration = new CmdLineOpts(args);
+        App app = new App(configuration);
+        app.run();
+        System.exit(0);
     }
 }
--- src/main/java/org/yb/cdc/CmdLineOpts.java
+++ src/main/java/org/yb/cdc/CmdLineOpts.java
@@ -1,18 +1,21 @@
 package org.yb.cdc;
 
-import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CmdLineOpts {
   private static final Logger LOGGER = LoggerFactory.getLogger(CmdLineOpts.class);
   
   private static final String DEFAULT_MASTER_ADDRESSES = "127.0.0.1:7100";
+
+  private CommandLine commandLine;
   
   private String masterAddresses;
+  private String tableName;
   private String streamId;
   private String tabletId;
   private String sslCertFile;
@@ -21,6 +24,14 @@ public class CmdLineOpts {
   private int maxTablets = 100;
   private long term;
   private long index;
+
+  public String getTableName() {
+    return this.tableName;
+  }
+
+  public void setTableName(String tableName) {
+    this.tableName = tableName;
+  }
 
   public long getTerm() {
     return this.term;
@@ -104,65 +115,79 @@ public class CmdLineOpts {
     this.maxTablets = maxTablets;
   }
 
-  public static CmdLineOpts createFromArgs(String[] args) {
+  public CommandLine getCommandLine() {
+    return this.commandLine;
+  }
+
+  public CmdLineOpts(String[] args) {
     Options options = new Options();
 
-    options.addOption("master-addresses", true, "List of YB master IPs to connect to");
+    options.addOption("master_addresses", true, "List of YB master IPs to connect to");
 
-    options.addOption("stream-id", true, "The stream ID on which we need to perform operations");
+    options.addOption("stream_id", true, "The stream ID on which we need to perform operations");
+
+    options.addOption("table_name", true, "Name of the table");
 
     options.addOption(
-        "get-checkpoint", false, "Option to specify that we need to print the checkpoints");
+        "get_checkpoint", false, "Option to specify that we need to print the checkpoints");
+    options.addOption("get_checkpoints", false, "Specify that we need checkpoints of all the tablets");
     
-    options.addOption("tablet-id", true, "The tablet ID on which we need to perform operations");
+    options.addOption("tablet_id", true, "The tablet ID on which we need to perform operations");
 
-    options.addOption("clean-all", false, 
+    options.addOption("clean_all", false, 
                       "Set the checkpoints of all the tablets for a given stream ID to OpId::Max");
 
-    options.addOption("set-checkpoint", true, "Checkpoint to set for a given tablet");
+    options.addOption("set_checkpoint", true, "Checkpoint to set for a given tablet");
     options.addOption("bootstrap", false, "Whether to bootstrap while setting checkpoint");  
     
     options.addOption(
-        "get-db-stream-info", false, "Return the DB stream information of a given stream");
+        "get_db_stream_info", false, "Return the DB stream information of a given stream");
 
-    options.addOption("ssl-cert-file", true, "Path to root certificate");
-    options.addOption("client-cert-file", true, "Path to client certificate");
-    options.addOption("client-key-file", true, "Path to client key file");
+    options.addOption("ssl_cert_file", true, "Path to root certificate");
+    options.addOption("client_cert_file", true, "Path to client certificate");
+    options.addOption("client_key_file", true, "Path to client key file");
 
-    options.addOption("max-tablets", true, "Number of tablets the yb-client should fetch");
+    options.addOption("max_tablets", true, "Number of tablets the yb-client should fetch");
 
     // Do the parsing
     CommandLineParser parser = new DefaultParser();
-    CommandLine commandLine = null;
-    
+    this.commandLine = null;
     try {
-        commandLine = parser.parse(options, args);
+        this.commandLine = parser.parse(options, args);
     } catch (Exception e) {
       LOGGER.error("Error while parsing arguments from command line: {}", e);
       System.exit(-1);
     }
 
-    CmdLineOpts configuration = new CmdLineOpts();
-    configuration.initialize(commandLine);
+    initialize(this.commandLine);
     
-    return configuration;
+    // return configuration;
   }
 
   private void initialize(CommandLine commandLine) {
-    if (commandLine.hasOption("master-addresses")) {
-      this.setMasterAddresses(commandLine.getOptionValue("master-addresses"));
+    if (commandLine.hasOption("master_addresses")) {
+      this.setMasterAddresses(commandLine.getOptionValue("master_addresses"));
     } else {
-      LOGGER.error("Please specify master addresses using the --master-addresses option");
+      LOGGER.error("Please specify master addresses using the --master_addresses option");
       System.exit(404);
     }
 
-    if (commandLine.hasOption("stream-id")) {
-      this.setStreamId(commandLine.getOptionValue("stream-id"));
+    if (commandLine.hasOption("stream_id")) {
+      this.setStreamId(commandLine.getOptionValue("stream_id"));
     } else {
-      LOGGER.error("Please specify stream ID using the --stream-id option");
+      LOGGER.error("Please specify stream ID using the --stream_id option");
       System.exit(404);
     }
 
-    
+    if (commandLine.hasOption("get_checkpoint")) {
+      if (!commandLine.hasOption("tablet_id")) {
+        LOGGER.error("No -tablet_id specified with get_checkpoint, if you want to get the checkpoints for all tablets, use -get_checkpoints");
+      }
+    }
+
+    if (commandLine.hasOption("get_checkpoint") && commandLine.hasOption("get_checkpoints")) {
+      LOGGER.error("Cannot specify both options -get_checkpoint and -get_checkpoints together");
+      System.exit(-1);
+    }
   }
 }
